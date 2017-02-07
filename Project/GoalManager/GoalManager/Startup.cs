7610662a42c06fd789fc00499350dafe906e7f6c
Andrew keller <trueleo1@gmail.com>
2017-02-07 18:30:50 -0500

    Creating Initial Project File

---
+++ Project/GoalManager/GoalManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoalManager.Startup))]
+namespace GoalManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
