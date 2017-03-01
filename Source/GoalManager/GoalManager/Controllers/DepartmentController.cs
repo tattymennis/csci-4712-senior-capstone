39c6791c23f14a7c9605604cca81fef70e7bbec9
Andrew Keller <trueleo1@gmail.com>
2017-03-01 08:59:48 -0500

    Addng view models to pages, specifically Departmentview model

--- Source/GoalManager/GoalManager/Controllers/DepartmentController.cs
+++ Source/GoalManager/GoalManager/Controllers/DepartmentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GoalManager.Data;
+using GoalManager.Models;
 
 namespace GoalManager.Controllers
 {
@@ -15,7 +17,31 @@ namespace GoalManager.Controllers
         }
         public ActionResult CreateDepartment()
         {
-            return View();
+            var vm = new DepartmentViewModel();
+            return View(vm);
+        }
+        [HttpPost]
+        public ActionResult CreateDepartment(Department tempDep)
+        {
+            /* if(tempDep.Location == "")
+            {
+                ModelState.AddModelError("Location", "Location cannnt be empty!"); //wrong
+            }
+            ModelState.AddModelError("Location", "Location Cannnt be empadf;ladf!"); //wrong
+            */
+
+            var tDBDep = new Department(); //optional
+            // if ModelState.isValid()
+            using (var db = new UserDBEntities())
+            {
+                db.Departments.Add(tDBDep);
+                db.SaveChangesAsync();
+            }
+            // 
+            RedirectToAction("/Home/Index");
+            DepartmentViewModel vm = new DepartmentViewModel();
+            vm.Department = tempDep;
+            return View(vm);
         }
         public ActionResult ModifyDepartment()
         {
