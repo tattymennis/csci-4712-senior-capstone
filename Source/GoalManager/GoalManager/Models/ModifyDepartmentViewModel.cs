b2b0102b4b16fa0e276d432dd2807ec663470fef
Andrew Keller <trueleo1@gmail.com>
2017-03-26 19:09:14 -0400

    Adding ModifyDpeartmentViewModel

---
+++ Source/GoalManager/GoalManager/Models/ModifyDepartmentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoalManager.Models
+{
+    public class ModifyDepartmentViewModel
+    {
+
+        public int DID { get; set; }
+        public string Name { get; set; }
+        
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int IDRef { get; set; }
+    }
+}
