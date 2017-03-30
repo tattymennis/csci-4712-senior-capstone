4ec392f411e310a27961c23599bfce99dbd275d5
Andrew Keller <Trueleo1@gmail.com>
2017-03-30 12:27:00 -0400

    Adding logic to quarters for Create Department

--- Source/GoalManager/GoalManager/Models/ModifyDepartmentViewModel.cs
+++ Source/GoalManager/GoalManager/Models/ModifyDepartmentViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoalManager.Models
 {
@@ -14,5 +15,43 @@ namespace GoalManager.Models
         public string Location { get; set; }
         public string Description { get; set; }
         public int IDRef { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Quarter1Name { get; set; }
+
+        [Required]
+        public DateTime Quarter1Start { get; set; }
+
+        [Required]
+        public DateTime Quarter1End { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Quarter2Name { get; set; }
+
+        [Required]
+        public DateTime Quarter2Start { get; set; }
+
+        [Required]
+        public DateTime Quarter2End { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Quarter3Name { get; set; }
+        [Required]
+        public DateTime Quarter3Start { get; set; }
+
+        [Required]
+        public DateTime Quarter3End { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Quarter4Name { get; set; }
+        [Required]
+        public DateTime Quarter4Start { get; set; }
+
+        [Required]
+        public DateTime Quarter4End { get; set; }
     }
 }
